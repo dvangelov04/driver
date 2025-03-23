2f099a640a6292dbdc5c3c40be7595bcf8edaa2e
Dimitar Angelov <162838518+dvangelov04@users.noreply.github.com>
2025-03-23 17:42:15 +0100

    tracker

---
+++ tracker.py
@@ -0,0 +1,81 @@
+import rclpy 
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan 
+
+import time
+
+
+# which topic should the message be published on 
+topicName = '/cmd_vel'
+subTopic = '/scan'
+
+
+class Tracker(Node):
+    def __init__(self):
+        super().__init__('simple')
+        self.publisherCreated = self.create_publisher(
+            Twist, 
+            topicName, 
+            100)
+
+
+        self.scan = self.create_subscription(
+            LaserScan,
+            subTopic,
+            self.PublisherCallBack,
+            100
+        )
+        self.counter = 0 
+        communicationPeriod = 1 
+
+        self.period = self.create_timer(communicationPeriod, self.PublisherCallBack)
+
+
+
+
+    def PublisherCallBack(self, msg):
+        twist_msg = Twist()
+        
+        centerIndex = len(msg.ranges) // 2
+        distanceAhead = msg.ranges[centerIndex]
+        
+        if distanceAhead < 1.0:
+            twist_msg.linear.x = 0.0
+            twist_msg.angular.z = 3.0
+        else:
+            twist_msg.linear.x = 3.0
+            twist_msg.angular.z = 0.0
+
+        if self.counter == 30:
+            twist_msg.linear.x = 0
+            self.publisherCreated.publish(twist_msg)
+        elif self.counter == 20: 
+            self.counter += 1
+            twist_msg.angular.z == 0.5
+            twist_msg.linear.x = 0.5
+            self.publisherCreated.publish(twist_msg)
+        else: 
+            self.counter += 1
+            twist_msg.angular.z == 1
+          
+            self.publisherCreated.publish(twist_msg)
+
+        # self.get_logger().info('Linear speed: ', "%s" twist_msg.linear.x)
+
+
+        
+def main(args=None):
+    rclpy.init(args=args)
+    publisherNode = Tracker()
+
+    rclpy.spin(publisherNode)
+
+    publisherNode.destroy_node()
+    rclpy.shutdown()
+
+
+
+
+if __name__ == '__main__':
+    main()
