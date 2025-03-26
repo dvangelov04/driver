eaeadc5521c0ca00f1fa83c4c40122db169348d4
Dimitar Angelov <162838518+dvangelov04@users.noreply.github.com>
2025-03-26 23:42:42 +0100

    left/right steer

--- tracker.py
+++ tracker.py
@@ -1,81 +1,72 @@
-import rclpy 
+import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan 
 
-import time
 
 
-# which topic should the message be published on 
-topicName = '/cmd_vel'
-subTopic = '/scan'
+# topics 
+
+# publisher topics
+steering = '/cmd_vel'
+
+#subscriber topics
+lidar = '/scan'
 
 
-class Tracker(Node):
+class Driver(Node): 
     def __init__(self):
-        super().__init__('simple')
-        self.publisherCreated = self.create_publisher(
-            Twist, 
-            topicName, 
-            100)
+        super().__init__('driverless')
+        self.publisher = self.create_publisher(Twist, steering, 100)
+        self.subscribe = self.create_subscription(LaserScan, lidar, self.velpub, 100)
 
-
-        self.scan = self.create_subscription(
-            LaserScan,
-            subTopic,
-            self.PublisherCallBack,
-            100
-        )
         self.counter = 0 
-        communicationPeriod = 1 
-
-        self.period = self.create_timer(communicationPeriod, self.PublisherCallBack)
-
-
-
-
-    def PublisherCallBack(self, msg):
-        twist_msg = Twist()
         
-        centerIndex = len(msg.ranges) // 2
-        distanceAhead = msg.ranges[centerIndex]
-        
-        if distanceAhead <= 5.0:
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 3.0
-        else:
-            twist_msg.linear.x = 3.0
-            twist_msg.angular.z = 0.0
 
-        if self.counter == 30:
-            twist_msg.linear.x = 0.0
-            self.publisherCreated.publish(twist_msg)
-        elif self.counter == 20: 
-            self.counter += 1
-            twist_msg.angular.z == 0.5
-            twist_msg.linear.x = 0.5
-            self.publisherCreated.publish(twist_msg)
+
+    def velpub(self, msg):
+        vel = Twist()
+        
+        centerAhead = msg.ranges[0]
+        leftAhead = msg.ranges[-15:0]
+        rightAhead = msg.ranges[0:15]
+
+        
+
+        if centerAhead <= 5:
+            vel.linear.x = 0.0
+            for i in leftAhead:
+                if i >= 5:
+                    pass
+                else: 
+                    vel.angular.z = -3.0
+
+            for i in rightAhead: 
+                if i >= 5:
+                    pass
+                else: 
+                    vel.angular.z = 3.0
+            
+        
         else: 
-            self.counter += 1
-            twist_msg.angular.z == 1.0
-          
-            self.publisherCreated.publish(twist_msg)
-
-        # self.get_logger().info('Linear speed: ', "%s" twist_msg.linear.x)
-
-
+            vel.linear.x = 2.0
         
+        self.publisher.publish(vel)
+
+
+
+
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Tracker()
-
+    node = Driver()
     rclpy.spin(node)
-
     node.destroy_node()
     rclpy.shutdown()
 
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
