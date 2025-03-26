162f16797a51e0efda195f2fb3013aa5aa5ca290
Dimitar Angelov <162838518+dvangelov04@users.noreply.github.com>
2025-03-27 00:57:12 +0100

    math trick ex

--- tracker.py
+++ tracker.py
@@ -2,48 +2,54 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import math
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan 
+
+
 
 # topics 
+
+# publisher topics
 steering = '/cmd_vel'
+
+#subscriber topics
 lidar = '/scan'
+
 
 class Driver(Node): 
     def __init__(self):
         super().__init__('driverless')
         self.publisher = self.create_publisher(Twist, steering, 100)
-        self.subscription = self.create_subscription(LaserScan, lidar, self.velpub, 100)
+        self.subscribe = self.create_subscription(LaserScan, lidar, self.velpub, 100)
+
+        self.counter = 0 
+        
+
 
     def velpub(self, msg):
         vel = Twist()
         
-        centerAhead = msg.ranges[0] if math.isfinite(msg.ranges[0]) else float('inf')
-        leftAhead = [r for r in msg.ranges[-30:] if math.isfinite(r)]
-        rightAhead = [r for r in msg.ranges[:30] if math.isfinite(r)]
+        centerAhead = msg.ranges[0]
+        left = [r for r in msg.ranges[-180:] if math.isfinite(r)]
+        right = [r for r in msg.ranges[:180] if math.isfinite(r)]
+        sum_right = float(sum(right), 5)
+        sum_left = float(sum(left), 5)
 
-        # Check for obstacles
-        left_blocked = any(r < 5.0 for r in leftAhead)
-        right_blocked = any(r < 5.0 for r in rightAhead)
-
-# Decision logic
-        if left_blocked and not right_blocked:
-            vel.angular.z = -3.0  # Turn right
-            vel.linear.x = 0.0
-            self.get_logger().info("Obstacle on the left — turning right.")
-        elif right_blocked and not left_blocked:
-            vel.angular.z = 3.0   # Turn left
-            vel.linear.x = 0.0
-            self.get_logger().info("Obstacle on the right — turning left.")
-        elif right_blocked and left_blocked:
-            vel.angular.z = 0.0   # No clear path
-            vel.linear.x = 0.0
-            self.get_logger().info("Blocked on both sides — stopping.")
+        if centerAhead <= 5:
+            if sum_left > sum_right:
+                vel.angular.z = 3.0
+            elif sum_right > sum_left:
+                vel.angular.z = -3.0
+            else: 
+                vel.linear.x = 0.0
+                self.publisher.publish(vel)
         else:
-            vel.linear.x = 3.0
-            vel.angular.z = 0.0
-            self.get_logger().info("Path is clear — moving forward.")
+        
+            vel.linear.x = 3.0 
+            self.publisher.publish(vel)
+        
 
-        self.publisher.publish(vel)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -51,6 +57,9 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+
+
 
 if __name__ == "__main__":
     main()
